c2fdc4391f77029f7c4b0136995fd91d390ef5ab
Tien <tn6609092@gmail.com>
2023-10-20 09:50:05 +0700

    tien

--- Demo/4/4.Designer.cs
+++ Demo/4/4.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "_4";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(259, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Tiến";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // _4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "_4";
+            Text = "_4";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
